6e87bb701b7264c6822340e7c3e89005f7919977
Samuel Eke <153599516+samuel-eke@users.noreply.github.com>
2025-04-08 14:49:06 +0100

    Delete middleware/errorHandler.js

--- middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const { logEvents } = require("./logger")
-
-const errHandler = function (err, req, res, next) { //nc
-    logEvents(`${err.name}:\t ${err.message}`, 'errorLogs.txt');
-    console.error(err.stack);
-    res.status(500).send(err.message);
-}
-
-module.exports = errHandler;
